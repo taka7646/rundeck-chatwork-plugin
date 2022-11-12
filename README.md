153abbe7a041da26eca8be89c65a0d48bf5f7b65
taka7646 <taka7646@gmail.com>
2022-11-12 17:05:18 +0900

    ・Jobオプションの置換実装 ・メッセージファイルの実装

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 This is a notification plugin.
 
-## JOB変数
-* {ID}
-* {JOB}
-* {STATUS}
-* {URL}
-* {USER}
+## JOB変数の置換
+* {ID} JobID
+* {JOB} Jobの名前
+* {STATUS} succeeded / failed 
+* {URL} JobのURL
+* {USER} Jobを実行したユーザー
+* Jobオプション
--- src/main/java/com/taka7646/rundeck/plugins/chatwork/ChatworkNotificationPlugin.java
+++ src/main/java/com/taka7646/rundeck/plugins/chatwork/ChatworkNotificationPlugin.java
@@ -6,6 +6,7 @@ import com.dtolabs.rundeck.core.plugins.configuration.PropertyScope;
 import com.dtolabs.rundeck.plugins.descriptions.PluginDescription;
 import com.dtolabs.rundeck.plugins.descriptions.PluginProperty;
 import com.dtolabs.rundeck.plugins.descriptions.Password;
+import com.dtolabs.rundeck.plugins.descriptions.TextArea;
 import java.util.*;
 import java.io.*;
 import java.net.HttpURLConnection;
@@ -24,18 +25,23 @@ public class ChatworkNotificationPlugin implements NotificationPlugin{
     @Password
     private String apiToken;
     @PluginProperty(name = "message", title = "送信メッセージ", description = "送信メッセージ", defaultValue = "", scope=PropertyScope.Instance)
+    @TextArea
     private String message;
+    @PluginProperty(name = "messageFile", title = "送信メッセージファイル", description = "送信するメッセージのファイル名を指定してください", defaultValue = "", scope=PropertyScope.Instance)
+    private String messageFile;
 
     public boolean postNotification(String trigger, Map executionData, Map config) {
         if ("".equals(this.message) || this.message == null) {
             return true;
         }
+        //System.err.println("config=" + config.toString() + ", exec=" + executionData.toString());
         this.send(this.createMessage(executionData, this.message));
         return true;
     }
 
     public String createMessage(Map executionData, String message)
     {
+        // メッセージを置換
         message = message.replace("\\n", "\n");
         message = message.replace("<br>", "\n");
         message = message.replace("{STATUS}", (String)executionData.get("status"));
@@ -43,6 +49,30 @@ public class ChatworkNotificationPlugin implements NotificationPlugin{
         message = message.replace("{URL}", (String)executionData.get("href"));
         message = message.replace("{ID}", executionData.get("id").toString());
         message = message.replace("{USER}", (String)executionData.get("user"));
+        // JOBオプションを置換
+        Map<String,Object> context = (Map<String,Object>)executionData.get("context");
+        Map<String,String> option = (Map<String,String>)context.get("option");
+        for (Map.Entry<String,String> e : option.entrySet()) {
+            message = message.replace("{"+e.getKey()+"}", e.getValue());
+        }
+        // メッセージファイルを追加
+        if (messageFile != null && !"".equals(messageFile)) {
+            File f = new File(messageFile);
+            try (BufferedReader br = new BufferedReader(new FileReader(f))) {
+                String text;
+                StringBuilder sb = new StringBuilder();
+                sb.append(message);
+                if (sb.length() > 0) {
+                    sb.append("\n");
+                }
+                while ((text = br.readLine()) != null) {
+                    sb.append(text).append("\n");
+                }
+                message = sb.toString();
+            } catch (Exception e) {
+                System.err.println("load file error:" + messageFile + "\n" + e.getMessage());
+            }
+        }
 
         return message;
     }
